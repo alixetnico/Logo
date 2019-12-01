7f150da5f4857c83456adb8f3924d5d492deb41a
NLTdev <NLT@NLTdev.com>
2019-12-01 18:46:21 +0100

    5

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<title>/logo</title>
+<title>Logo /// alixetnico.com</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<h1>/logo</h1>
+<h1>Logo</h1>
 <h2><a href="/logo/blanc.jpg">blanc.jpg</a></h2>
 <h2><a href="/logo/blanc.png">blanc.png</a></h2>
 <h2><a href="/logo/noir.jpg">noir.jpg</a></h2>
 <h2><a href="/logo/noir.png">noir.png</a></h2>
+<hr>
+<h2><a href="https://www.alixetnico.com/">alixetnico.com</a></h2>
 </html>
